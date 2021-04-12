f3c4a3f39b7791b8c8bf590e2acf8983a9ce1a95
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2021-04-12 18:36:14 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,138 @@
+[![GitHub Workflow Status](https://img.shields.io/github/workflow/status/olegtarasov/WebSocketWrapper/Build%20and%20publish?style=flat-square)](https://github.com/olegtarasov/WebSocketWrapper/actions)
+[![Nuget](https://img.shields.io/nuget/v/WebSocketWrapper?style=flat-square)](https://www.nuget.org/packages/WebSocketWrapper)
+[![Donwloads](https://img.shields.io/nuget/dt/WebSocketWrapper?label=Nuget&style=flat-square)](https://www.nuget.org/packages/WebSocketWrapper)
+
+# WebSocketWrapper
+
+This is a thin wrapper on top of .NET Standard `System.Net.WebSockets.ClientWebSocket` with the following feautures:
+
+* Receive messages as an `IAsyncEnumerable`.
+* Automatic reconnect. Currently, this library tries to automatically restore the connection no matter what (but uses sensible incremental intervals).
+* Optional watchdog timer, which triggers a reconnect if no message is reveived for a specified period of time.
+
+Please be aware that the library was just extracted from a different project of mine, and doesn't cover a lot of use cases. Specifically:
+
+* ❗️ Sending and receiving of only text messages is supported.
+* ❗️ Receiving messages is possible only through `IAsyncEnumerable`.
+* ❗️ Automatic reconnect can't be turned off at the moment (but you can handle the `Reconnected` event and `Close()` the socket as a workaround).
+
+## What's new
+
+### `1.0.0`
+
+* First version, just extracted from a different project.
+
+## Usage
+
+### Reconnect policy
+
+This wrapper tries to maintain the connection no matter what. When an error happens, reconnect process is triggered:
+
+* At first, it tries to reconnect immediately.
+* If that fails, a progressive cooldown is used: the wrapper waits for 1, 2, 4, 8, 16, 32 or 64 seconds until connection is established.
+* After reaching 64 seconds, cooldown time stops increasing and the wrapper tries to reconnect every 64 seconds. 
+
+### Connecting
+
+Use `Connect()` to initiate the connection. This method uses standard reconnect logic until it's connected. `false` is returned only
+if connection process is cancelled with a token or something critical happens.
+
+```c#
+private async Task ConnectAndReceive()
+{
+    bool connected = await _socket.ConnectAsync(new Uri("wss://stream.binance.com:9443/stream"));
+}
+```
+
+### Sending messages
+
+Use `SendMessageAsync` to send a text message. Only text messages are supported currently.
+
+```c#
+await _socket.SendMessageAsync(request);
+```
+
+### Receiving messages
+
+Call `ReceiveMessagesAsync` in `await foreach` to process messages as they arrive. You don't need to handle connection
+failures, everything is handled behind the scenes.
+
+```c#
+private async Task ConnectAndReceive()
+{
+    bool connected = await _socket.ConnectAsync(new Uri("wss://stream.binance.com:9443/stream"));
+    
+    Task.Run(async () => await ReceiveMessagesAsync()); // Start receiving.
+}
+
+private async Task ReceiveMessagesAsync()
+{
+    try
+    {
+        await foreach (string msg in _socket.ReceiveMessagesAsync(_tokenSource.Token))
+        {
+            // Process the message
+        }
+        
+        // No need to close the socket, it's in an aborted state after we cancelled receiving.
+    }
+    catch (TaskCanceledException)
+    {
+        // Cancellation token was fired
+    }
+    catch (Exception e)
+    {
+        // Some other critical error
+    }
+}
+```
+
+### Using watchdog
+
+You can use a watchdog timer to monitor for incoming messages. This is useful for some quirky servers which don't drop the connection but instead
+just stop sending you messages. You can specify a time interval which will trigger a reconnect if no messages are received in that interval.
+
+Watchdog is turned off by default, you can arm it using `StartWatchdog()` method:
+
+```c#
+_socket.StartWatchdog(5);
+```
+
+Be aware that watchdog doesn't rearm automatically after a reconnect. You need to arm it again in `Reconnected` event handler.
+
+### Handling reconnects
+
+You can use a `Reconnected` event, which is fired after a socket succesfully restored connection. You can use this handler to
+rearm a watchdog timer for example.
+
+```c#
+_socket.Reconnected += async reason =>
+{
+    _logger?.LogInformation($"WebSocket is being reconnected for reason: {reason}");
+    _socket.StartWatchdog(5);
+};
+```
+
+### Closing the socket
+
+You can close the socket at any time with `CloseAsync()` method.
+
+### Logging support
+
+`ClientWebSocketWrapper` uses a standard .NET Core logging abstractions. You can turn logging on by providing an
+instance of `Microsoft.Extensions.Logging.ILoggerFactory`. In this example we use Serilog with console sink.
+
+You can also inject your standard `IloggerFactory` through .NET Core DI.
+
+```c#
+// Add the following Nuget packages to your project:
+// * Serilog.Sinks.Console
+// * Serilog.Extensions.Logging 
+
+Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console(theme: ConsoleTheme.None)
+                .CreateLogger();
+
+var socket = new ClientWebSocketWrapper(new SerilogLoggerFactory());
+```
---
+++ WebSocketWrapper/ClientWebSocketWrapper.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.WebSockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace WebSocketWrapper
+{
+    /// <summary>
+    /// A thin wrapper on top of <see cref="ClientWebSocket"/> with <see cref="IAsyncEnumerable"/> receiving,
+    /// automatic reconnections and watchdog.
+    /// </summary>
+    public class ClientWebSocketWrapper : IDisposable, IAsyncDisposable
+    {
+        private static readonly Encoding Utf8 = Encoding.UTF8;
+
+        private readonly ILogger _logger;
+
+        private Uri _uri;
+        private ClientWebSocket _socket;
+        private Timer _watchdog;
+        private TimeSpan _watchdogTimeout;
+        private bool _watchdogTriggered = false;
+        private ManualResetEventSlim _watchdogReconnectedEvent;
+        private DateTime _lastMessageTime;
+        private int _reconnectInterval = 0;
+
+        /// <summary>
+        /// Fired when websocket reconnected for some reason.
+        /// </summary>
+        public event Action<ReconnectReason> Reconnected;
+
+        /// <summary>
+        /// Ctor.
+        /// </summary>
+        /// <param name="loggerFactory">Optional logger factory to enable logging.</param>
+        public ClientWebSocketWrapper(ILoggerFactory loggerFactory = null)
+        {
+            _logger = loggerFactory?.CreateLogger<ClientWebSocketWrapper>();
+        }
+
+        /// <summary>
+        /// Connect to a specified URI.
+        /// </summary>
+        /// <param name="uri">URI to connect to.</param>
+        /// <param name="token">Optional cancellation token to cancel the connection process.</param>
+        /// <returns><see cref="true"/> if connected successfully, <see cref="false"/> otherwise.</returns>
+        public async Task<bool> ConnectAsync(Uri uri, CancellationToken token = default)
+        {
+            _logger?.LogDebug($"Connecting to {uri}");
+            _uri = uri;
+            _reconnectInterval = 0;
+
+            return await ReconnectAsync(token);
+        }
+
+        /// <summary>
+        /// Starts a watchdog timer. The timer fires every second and checks the time the last message was received.
+        /// If more than <see cref="timeout"/> elapsed from that time, websocket reconnect is triggered.
+        /// </summary>
+        /// <param name="timeout">The amount of time that should elapse since the last message to trigger the reconnect.</param>
+        public void StartWatchdog(TimeSpan timeout)
+        {
+            _logger?.LogInformation($"Starting watchdog with timeout of {timeout}.");
+            _watchdog?.Dispose();
+            _watchdogTriggered = false;
+            _watchdogTimeout = timeout;
+            _lastMessageTime = DateTime.UtcNow;
+            _watchdog = new Timer(OnWatchdog, null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
+        }
+
+        /// <summary>
+        /// Close the socket.
+        /// </summary>
+        /// <param name="token">An optional cancellation token to cancel the closing process.</param>
+        /// <returns><see cref="true"/> if closed successfully, <see cref="false"/> otherwise.</returns>
+        public async Task<bool> CloseAsync(CancellationToken token = default)
+        {
+            if (_socket == null)
+                return true;
+            
+            if (_socket.State != WebSocketState.Open && _socket.State != WebSocketState.CloseReceived &&
+                _socket.State != WebSocketState.CloseSent)
+            {
+                _logger?.LogWarning($"Invalid socket state, can't close: {_socket.State}");
+                return true;
+            }
+            
+            _logger?.LogDebug($"Closing connection");
+            try
+            {
+                await _socket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, token);
+                return true;
+            }
+            catch (TaskCanceledException)
+            {
+                return false;
+            }
+            catch (Exception e)
+            {
+                _logger?.LogError(e, $"Failed to close websocket: {e.Message}");
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Sends a string message through the websocket.
+        /// </summary>
+        /// <param name="message">A message to send.</param>
+        /// <param name="token">An optional cancellation token to cancel the sending process.</param>
+        /// <returns><see cref="true"/> if sent successfully, <see cref="false"/> otherwise.</returns>
+        public async Task<bool> SendMessageAsync(string message, CancellationToken token = default)
+        {
+            try
+            {
+                var buff = Utf8.GetBytes(message);
+                await _socket.SendAsync(new ArraySegment<byte>(buff), WebSocketMessageType.Text, true, token);
+            }
+            catch (TaskCanceledException)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Receive text messages as an async enumerable.
+        /// </summary>
+        /// <param name="token">An optional cancellation token to cancel the receiving.</param>
+        /// <returns>An <see cref="IAsyncEnumerable{T}"/> which produces one full received text message at a time.</returns>
+        public async IAsyncEnumerable<string> ReceiveMessagesAsync([EnumeratorCancellation] CancellationToken token = default)
+        {
+            var buffer = new ArraySegment<byte>(new byte[4096]);
+            await using var mem = new MemoryStream();
+
+            // Loop over messages
+            while (!token.IsCancellationRequested)
+            {
+                // Loop over message parts
+                WebSocketReceiveResult result;
+                int total = 0;
+                do
+                {
+                    if (_socket.State != WebSocketState.Open)
+                    {
+                        _logger?.LogError($"Socket state is: {_socket.State}. Reconnecting.");
+                        if (!await ReconnectAsync(token))
+                            yield break;
+
+                        mem.SetLength(0);
+                        OnReconnected(ReconnectReason.InvalidState);
+
+                        break;
+                    }
+
+                    try
+                    {
+                        result = await _socket.ReceiveAsync(buffer, token);
+                        _lastMessageTime = DateTime.UtcNow;
+                    }
+                    catch (TaskCanceledException)
+                    {
+                        _logger?.LogInformation("ReceiveAsync has been cancelled.");
+                        yield break;
+                    }
+                    catch (Exception e)
+                    {
+                        if (_watchdogTriggered)
+                        {
+                            _logger?.LogDebug("Exited ReceiveAsync due to the watchdog. Waiting for signal.");
+                            try
+                            {
+                                _watchdogReconnectedEvent.Wait(token);
+                            }
+                            catch (TaskCanceledException)
+                            {
+                                _logger?.LogDebug("Waiting for watchdog signal has been cancelled.");
+                                yield break;
+                            }
+
+                            _logger?.LogDebug("Got a signal after watchdog reconnect. Resuming ReceiveAsync.");
+                            mem.SetLength(0);
+                            break;
+                        }
+
+                        _logger?.LogError(e, $"Unhandled websocket error: {e.Message}. Will try to reconnect.");
+                        if (!await ReconnectAsync(token))
+                            yield break;
+                            
+                        mem.SetLength(0);
+                        OnReconnected(ReconnectReason.SocketError);
+
+                        break;
+                    }
+
+                    if (result.MessageType == WebSocketMessageType.Close)
+                    {
+                        mem.SetLength(0);
+                        
+                        _logger?.LogWarning($"Remote connection close requested. Close status: {result.CloseStatus}, description: {result.CloseStatusDescription}. Closing connection and trying to reconnect.");
+                        if (!await CloseAsync(token) || !await ReconnectAsync(token))
+                            yield break;
+
+                        OnReconnected(ReconnectReason.ClosedByServer);
+                        
+                        break;
+                    }
+
+                    mem.Write(buffer[..result.Count]);
+                    checked
+                    {
+                        total += result.Count;
+                    }
+                } while (!result.EndOfMessage);
+
+                if (mem.Length == 0)
+                    continue;
+
+                string message = Utf8.GetString(mem.GetBuffer()[..total]);
+                mem.SetLength(0);
+
+                yield return message;
+            }
+        }
+
+        private async Task<bool> ReconnectAsync(CancellationToken token = default)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                if (_reconnectInterval > 0)
+                {
+                    _logger?.LogInformation($"Waiting for {_reconnectInterval} seconds before reconnecting");
+                    try
+                    {
+                        await Task.Delay(TimeSpan.FromSeconds(_reconnectInterval), token);
+                    }
+                    catch (TaskCanceledException)
+                    {
+                        return false;
+                    }
+                }
+
+                if (_socket != null)
+                {
+                    if (_socket.State == WebSocketState.Open)
+                    {
+                        if (! await CloseAsync(token))
+                            return false;
+                    }
+                
+                    _socket.Dispose();
+                }
+            
+                _socket = new ClientWebSocket();
+                
+                try
+                {
+                    await _socket.ConnectAsync(_uri, token);
+                    _reconnectInterval = 0;
+                    _logger?.LogInformation("Connected");
+                    return true;
+                }
+                catch (TaskCanceledException)
+                {
+                    return false;
+                }
+                catch (Exception e)
+                {
+                    _logger?.LogError(e, $"Failed to connect: {e.Message}");
+                    if (_reconnectInterval < 64)
+                        _reconnectInterval = _reconnectInterval > 0 ? _reconnectInterval * 2 : 1;
+                    _logger?.LogInformation($"Reconnect interval is: {_reconnectInterval} seconds.");
+                }
+            }
+
+            return false;
+        }
+
+        private void OnWatchdog(object state)
+        {
+            if (DateTime.UtcNow - _lastMessageTime >= _watchdogTimeout)
+            {
+                _watchdog.Dispose();
+                _watchdog = null;
+                
+                _logger?.LogError($"Watchdog has been triggered! The interval was {_watchdogTimeout}.");
+                _watchdogTriggered = true;
+
+                _watchdogReconnectedEvent = new ManualResetEventSlim();
+                _socket.Dispose();
+                _socket = null;
+                Task.Run(async () => await ReconnectAsync()).ContinueWith(t =>
+                {
+                    _watchdogReconnectedEvent.Set();
+                    OnReconnected(ReconnectReason.Watchdog);
+                });
+            }
+        }
+
+        public void Dispose()
+        {
+            _socket?.Dispose();
+            _socket = null;
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            Dispose();
+            return new ValueTask(Task.CompletedTask);
+        }
+
+        protected virtual void OnReconnected(ReconnectReason reason)
+        {
+            Reconnected?.Invoke(reason);
+        }
+    }
+
+    /// <summary>
+    /// The reason behind a reconnect.
+    /// </summary>
+    public enum ReconnectReason
+    {
+        /// <summary>
+        /// Socket was closed by server.
+        /// </summary>
+        ClosedByServer,
+        
+        /// <summary>
+        /// Underlying websocket became invalid.
+        /// </summary>
+        InvalidState,
+        
+        /// <summary>
+        /// Reconnect was triggered by a watchdog because no data was received in a specified timeframe.
+        /// </summary>
+        Watchdog,
+        
+        /// <summary>
+        /// Some other socket error.
+        /// </summary>
+        SocketError
+    }
+}
